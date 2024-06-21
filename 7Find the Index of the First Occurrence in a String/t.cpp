158198d3327cdebf5e2b54963e613a31dcef8179
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-06-21 08:54:24 +0600

    Add files via upload

---
+++ 7Find the Index of the First Occurrence in a String/t.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Paste your class and function definitions here
+
+
+class Solution {
+public:
+    int strStr(string h, string n) {
+        int hs=h.size();
+        int ns=n.size();
+
+        if(hs==1){
+            return 0;
+        }else{
+                for(int i=0;i<=hs-ns;i++){
+                if(h.substr(i,ns)==n){
+                return i;
+                }
+            }
+        }
+    return -1;
+    }
+};
+
+
+
+
+int main() {
+    Solution s;
+    auto ans = s.strStr("abc","c");
+    cout << ans << endl;
+    // Add more test cases as needed
+    return 0;
+}
