ce3b614eb290c8855cfe4f8880ba1510676386f4
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-06-22 22:56:46 +0600

    Add files via upload

---
+++ 11Sqrt(x)/m.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Paste your class and function definitions here
+
+
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int ans=sqrt(x);
+        ans=floor(ans);
+    return ans;
+    }
+    
+};
+
+
+
+int main() {
+    Solution s;
+    auto ans = s.
+    cout << ans << endl;
+    // Add more test cases as needed
+    return 0;
+}
