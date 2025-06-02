53c4504cfe3c730fb3d22764ee5a0b0b2c880f64
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2025-06-02 20:28:05 +0600

    Add files via upload

---
+++ 67Remove Nth Node From End of List/m.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Paste here
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode * res = new ListNode(0, head);
+        ListNode * dummy = res;
+
+        for(int i = 0; i < n; i++) {
+            head = head->next;  
+        }
+        while(head){
+            head = head->next;
+            dummy = dummy->next;
+        }
+        dummy->next = dummy->next->next;
+        return res->next;
+
+    }
+};
+
+
+
+signed main() {
+    Solution s;
+    
+    return 0;
+}
