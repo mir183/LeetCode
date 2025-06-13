61ce27c0de511acbea22bcfbad2c1a3865af0746
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2025-06-13 21:08:44 +0600

    Add files via upload

---
+++ 74Maximum Depth of Binary Tree/m.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Paste here
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
+    }
+};
+
+
+
+signed main() {
+    Solution s;
+    
+    return 0;
+}
